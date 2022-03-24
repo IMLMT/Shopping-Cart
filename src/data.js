cee08cadc26882f9adae58bb6b42979e74f290dd
IILMTII\Mathew <lovinmathew@hotmail.com>
2022-03-23 22:06:18 -0500

    Rudimentary shopping cart

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [{
+            id: '1',
+            name: 'H‑E‑B Select Ingredients Family Size Homestyle Lasagna 38 oz',
+            price: 8.22,
+            image: 'https://images.heb.com/is/image/HEBGrocery/001812540-1?id=ydFR73&fmt=jpg&dpr=off&fit=constrain,1&wid=296&hei=296'
+        },
+        {
+            id: '2',
+            name: 'H‑E‑B Select Ingredients Riced Original Cauliflower 12 oz',
+            price: 2.35,
+            image: 'https://images.heb.com/is/image/HEBGrocery/002233669-1?id=93eST2&fmt=jpg&dpr=off&fit=constrain,1&wid=296&hei=296'
+        },
+        {
+            id: '3',
+            name: 'H‑E‑B Organics Mixed Vegetables 16 oz',
+            price: 2.55,
+            image: 'https://images.heb.com/is/image/HEBGrocery/001968486-1?id=eEfSX1&fmt=jpg&dpr=off&fit=constrain,1&wid=296&hei=296'
+        },
+    ]
+}
+
+export default data
